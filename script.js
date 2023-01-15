d6858b63576bbd3a164060a37b6d00f4aa46ace0
Lokesh Raj <32189830+aQwus@users.noreply.github.com>
2023-01-16 02:08:43 +0530

    updated without firebase

--- script.js
+++ script.js
@@ -31,14 +31,47 @@
 // enterPressed();
 
 function foo () {
-	const queryString = window.location.search;
-	console.log(queryString);
 
-	const urlParams = new URLSearchParams(queryString);
-	const dr = urlParams.get('dr')
-	console.log(dr);
+    const queryString = window.location.search;
 
-	document.getElementById("dr").innerText = dr;
+    const urlParams = new URLSearchParams(queryString);
+    const v = urlParams.get('dr');
+    console.log(v);
+
+    document.getElementById('dr').innerText = v;
+
+    var picker = new AppointmentPicker(document.getElementById('time'), {
+        interval: 30,
+        mode: '12h',
+        minTime: 11,
+        maxTime: 20,
+        startTime: 09, // time<10 needs trailing zero
+        endTime: 23,
+        disabled: ['1:00 pm', '4:00 pm', '5:30 pm']
+    });
+
+}
+
+function foo2 () {
+    document.getElementById('loader').style.display = 'inline';
+    var sheetAppUrl = 'https://script.google.com/macros/s/AKfycbzvZvWaXBp_yioVKpgCA-41TWq7rG9-CXV-udCGBYxgJymFn5DqPZc30mVBFsK4CUTKIA/exec';
+    //var $form = $('form#test-form');
+    console.log(sheetAppUrl);
+
+    const form = document.forms['submit-to-google-sheet']
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+    })
+
+    fetch(sheetAppUrl, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            console.log('Success!', response);
+            document.getElementById('email').value = '';
+            document.getElementById('name').value = '';
+            document.getElementById('loader').style.display = 'none';
+        })
+        .catch(error => console.error('Error!', error.message))
+
 }
 
 document.addEventListener('DOMContentLoaded', foo);
